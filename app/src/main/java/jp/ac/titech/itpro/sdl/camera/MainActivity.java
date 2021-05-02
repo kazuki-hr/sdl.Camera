c65fc5c9df752b40fa0cecf4732d0abc75b5ed91
Kazuki Hirohata <hirohata.k.aa@m.titech.ac.jp>
2021-05-02 20:49:09 +0900

    フルサイズ画像ファイル版

--- app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/camera/MainActivity.java
@@ -5,20 +5,28 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private final static int REQ_PHOTO = 1234;
-    private Bitmap photoImage = null;
+    private File currentPhotoFile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +41,22 @@ public class MainActivity extends AppCompatActivity {
             @SuppressLint("QueryPermissionsNeeded")
             List<ResolveInfo> activities = manager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
             if (!activities.isEmpty()) {
-                startActivityForResult(intent, REQ_PHOTO);
+                // Create the File where the photo should go
+                currentPhotoFile = null;
+                try {
+                    currentPhotoFile = createImageFile();
+                } catch (IOException ex) {
+                    // Error occurred while creating the File
+                    Toast.makeText(MainActivity.this, R.string.cannot_create_image_file, Toast.LENGTH_LONG).show();
+                }
+                // Continue only if the File was successfully created
+                if (currentPhotoFile != null) {
+                    Uri photoURI = FileProvider.getUriForFile(this,
+                            "jp.ac.titech.itpro.sdl.camera.fileprovider",
+                            currentPhotoFile);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(intent, REQ_PHOTO);
+                }
             } else {
                 Toast.makeText(MainActivity.this, R.string.toast_no_activities, Toast.LENGTH_LONG).show();
             }
@@ -41,22 +64,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void showPhoto() {
-        if (photoImage == null) {
+        if (currentPhotoFile == null) {
             return;
         }
         ImageView photoView = findViewById(R.id.photo_view);
-        photoView.setImageBitmap(photoImage);
+        photoView.setImageURI(Uri.fromFile(currentPhotoFile));
     }
 
     @Override
     protected void onActivityResult(int reqCode, int resCode, Intent data) {
         super.onActivityResult(reqCode, resCode, data);
-        if (reqCode == REQ_PHOTO) {
-            if (resCode == RESULT_OK) {
-                Bundle extras = data.getExtras();
-                photoImage = (Bitmap) extras.get("data");
-            }
+        if (reqCode != REQ_PHOTO && resCode != RESULT_OK) {
+            currentPhotoFile = null;
         }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        return File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
     }
 
     @Override
